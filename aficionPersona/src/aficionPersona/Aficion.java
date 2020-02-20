143ea148ca2ce768c15bdb648f2aee23789cf602
DBelda <davidbelda00@gmail.com>
2020-02-20 10:19:36 +0100

    v2.2

--- aficionPersona/src/aficionPersona/Aficion.java
+++ aficionPersona/src/aficionPersona/Aficion.java
@@ -1,5 +1,51 @@
 package aficionPersona;
 
+/**
+ *
+ *
+ */
 public class Aficion {
-
-}
+    
+   // Variables 
+   private String [] Aficiones = new String [10];
+   private int length;
+   
+   // Constructor
+   public Aficion(String []aficionesArray){
+       this.Aficiones = aficionesArray;
+       this.length = this.Aficiones.length;
+   }
+   
+   
+    //Obtener Aficiones
+   public String[] getAficiones(){
+       return this.Aficiones;
+       
+   }
+   
+   //Obtener longitud
+   public int getLength(){
+       return this.length;
+   }
+    
+   
+   //Ver si son afines 
+   public boolean  sonAfines (Aficion aficionesArray){
+       int aficionesAfines = 0;
+       for (int i = 0; i < this.length; i++){
+           for(int j = 0; j < aficionesArray.getLength(); j++) {
+               if (this.Aficiones[i]== aficionesArray.getAficiones()[j])
+                   aficionesAfines++;
+           }
+       }
+       
+       if (aficionesAfines >=3)
+           return true;
+       
+       return false;
+           
+           
+           
+           
+       }
+   }
--- aficionPersona/src/aficionPersona/Persona.java
+++ aficionPersona/src/aficionPersona/Persona.java
@@ -1,5 +1,38 @@
 package aficionPersona;
 
+
 public class Persona {
 
-}
+    Aficion misAficiones;
+    private boolean afin;
+    
+    
+    public Persona (String[] aficionesArray){
+        this.misAficiones = new Aficion(aficionesArray);
+        this.afin = false;
+    }
+
+    
+    public Aficion getAficionClase(){
+        return this.misAficiones;
+    }
+    
+   
+    public String esAfin(){
+        if (this.afin)
+            return " las 2 personas son afines";
+        else
+            return " las 2 personas no son afines";
+    }
+    
+    
+    public void verAfinidad (Aficion aficionesArray){
+        if(this.afin && this.misAficiones.sonAfines(aficionesArray)){
+            this.afin = true;
+            
+        }
+    }
+   
+        
+    }
+    
