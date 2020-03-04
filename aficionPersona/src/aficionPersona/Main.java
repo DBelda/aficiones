ea95e9bec056e5c087ac5f682174dfa4834eeaf1
DBelda <davidbelda00@gmail.com>
2020-03-04 08:11:20 +0100

    v3.1

--- aficionPersona/src/aficionPersona/Main.java
+++ aficionPersona/src/aficionPersona/Main.java
@@ -1,5 +1,54 @@
 package aficionPersona;
 
-public class Main {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
-}
+public class Main {
+	
+	final static Aficion basket = new Aficion("basket");
+	final static Aficion futbol = new Aficion("futbol");
+	final static Aficion bricolaje = new Aficion("bricolaje");
+	final static Aficion pintar = new Aficion("pintar");
+	final static Aficion leer = new Aficion("leer");
+	final static Aficion cantar = new Aficion("cantar");
+	final static Aficion escribir = new Aficion("escribir");
+	final static Aficion programar = new Aficion("programar");
+	final static Aficion natacion = new Aficion("natacion");
+	final static Aficion bolos = new Aficion("bolos");
+	final static Aficion golf = new Aficion("golf");
+	
+	
+
+	public static void main(String[] args) {
+		final Scanner sc = new Scanner(System.in);
+		
+		final Persona[] personas = {
+			new	Persona("Pepe", new Aficion[]{futbol, cantar, leer, bolos, golf, escribir, pintar}),
+			new	Persona("David", new Aficion[]{programar, cantar, natacion, bricolaje, basket}),
+			new	Persona("Delta", new Aficion[]{futbol, cantar, programar, bolos, pintar})
+		};
+		
+		final List<Persona> lista = Arrays.asList(personas);
+		
+		System.out.println("Selecciona por número la persona con la que quieres ver las afinidades.");
+		System.out.println("Las personas elegibles son :" );
+		lista.forEach(System.out::println);
+		System.out.print("Código : ");
+		
+		int cod = Integer.parseInt(sc.nextLine());
+		Persona selectedP = lista.stream().filter(p -> p.getCodPersona() == cod).findFirst().orElse(null);
+		if (selectedP == null) {
+			System.out.println("No existe la persona");
+			return;
+		}
+		
+		lista.forEach(p -> {
+			if (p.equals(selectedP))
+				return;
+			boolean sonAfines = Persona.sonAfines(p, selectedP);
+			System.out.printf("%s y %s %s afines\n", p.getNombre(), selectedP.getNombre(), sonAfines ? "son" : "no son"); 
+			// %s significa cadena de carácteres
+		});
+	}
+
